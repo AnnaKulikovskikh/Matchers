20452220ac9ec4f574ea2e957db3ddcde06a5990
Anna <k_anna_i@mail.ru>
2021-08-18 20:59:16 +0300

    ssh

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,16 @@
+import sorting from '../app';
+
+test('health', () => {
+  const heroes = [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+  ];
+  const expected = [
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+    {name: 'мечник', health: 10},
+  ];
+  const result = sorting(heroes);
+  expect(result).toEqual(expected);
+});
