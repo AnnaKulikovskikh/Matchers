ae993c1f1e0e01c278dfe27e3428b9cc3083a770
Anna <k_anna_i@mail.ru>
2021-08-22 00:01:20 +0300

    210821

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -14,3 +14,18 @@ test('health', () => {
   const result = healthSort(heroes);
   expect(result).toEqual(expected);
 });
+
+test('toBe', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const result = healthSort(heroes);
+  expect(result).toBe(expected);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
 // TODO: write your code here
 export default function healthSort(heroes) {
-  return heroes.sort((a, b) => a.health < b.health ? 1 : -1);
+  return heroes.sort((a, b) => {
+    if (a.health < b.health) { return 1; }
+    return -1;
+  });
 }
